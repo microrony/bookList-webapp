cc0858c411ad3ca035b58b8a961726198bc88302
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-09-25 12:18:04 +0600

    update

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// Book Class: Represents a Book
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn - isbn;
+  }
+}
+
+// UI Class: Handle UI Tasks
+
+// Store Class: Handles Storage
+
+// Events Display Books
+
+// Events Add a Book
+
+// Events Remove a Book
